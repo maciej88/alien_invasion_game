36f0712f91ecdd213cbdce3ea402820fd29bf21a
maciej <mkrywult88@gmail.com>
2022-02-11 19:42:08 +0100

    background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,12 +9,15 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("ALIEN INVASION!!!")
 
+        self.bg_color = (0, 0, 255)
+
     def run_game(self): #main loop and game
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.bg_color) #background color refresh
             pygame.display.flip() #last modified window (view)
 
 if __name__ == '__main__':
