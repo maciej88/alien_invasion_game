996601393c4fb6e55d72617cab696a7d86c5c99c
maciej <mkrywult88@gmail.com>
2022-02-12 19:42:55 +0100

    speed accelerate to 1.5 px

--- ship.py
+++ ship.py
@@ -8,20 +8,25 @@ class Ship:
 
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()  # rect as rectangle
 
         self.rect.midbottom = self.screen_rect.midbottom  # in middle of bottom window
 
+        self.x = float(self.rect.x)
+
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         # ship view
