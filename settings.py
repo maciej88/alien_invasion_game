886ff67f2ed5f78fc36f8ff3d1458af8a11e0b5a
maciej <mkrywult88@gmail.com>
2022-02-18 22:24:02 +0100

    number of ships lives = 3 -> settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,9 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (0, 0, 255)
+
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         #bullets stettings:
         self.bullet_speed = 1.5
