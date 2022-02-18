271edae5903e425ee2f55c6f78129aa86d056325
maciej <mkrywult88@gmail.com>
2022-02-18 22:22:36 +0100

    statistic data collection from game - game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -1,1 +1,8 @@
 class GameStats:
+    #ingame statistic
+    def __init__(self, ai_game):
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_starts(self):
+        self.ships_left = self.settings.ship_limit
