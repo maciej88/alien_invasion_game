d28dd4e4f654c9afc81995907bb6113408ec683d
maciej <mkrywult88@gmail.com>
2022-02-22 17:18:08 +0100

    actual level number

--- scoreboard.py
+++ scoreboard.py
@@ -12,6 +12,7 @@ class Scoreboard:
         self.font = pygame.font.SysFont(None, 48)
 
         self.prep_score()
+        self.prep_level()
 
         self.prep_high_score()
 
@@ -30,6 +31,7 @@ class Scoreboard:
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     def prep_high_score(self):
         high_score = round(self.stats.high_score, -1)
@@ -45,3 +47,12 @@ class Scoreboard:
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
+
+    def prep_level(self):
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(
+            level_str, True, self.text_color, self.settings.bg_color
+        )
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
