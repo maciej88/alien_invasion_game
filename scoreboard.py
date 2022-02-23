fc92a40560c49e9f70d4174ea9dbaee61968fcc9
maciej <mkrywult88@gmail.com>
2022-02-23 16:58:29 +0100

    function for calculating lives left

--- scoreboard.py
+++ scoreboard.py
@@ -1,8 +1,12 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 class Scoreboard:
 
     def __init__(self, ai_game):
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -13,6 +17,7 @@ class Scoreboard:
 
         self.prep_score()
         self.prep_level()
+        self.prep_ships()
 
         self.prep_high_score()
 
@@ -56,3 +61,11 @@ class Scoreboard:
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
