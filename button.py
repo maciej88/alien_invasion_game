90b9095e683064ce7b19d5d62d41e9a7e61e55ec
maciej <mkrywult88@gmail.com>
2022-02-27 16:54:14 +0100

    button comments

--- button.py
+++ button.py
@@ -3,6 +3,8 @@ import pygame.font
 class Button():
 
     def __init__(self, ai_game, msg):
+        """button attributes initialization"""
+
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
 
@@ -20,6 +22,8 @@ class Button():
         self._prep_msg(msg)
 
     def _prep_msg(self, msg):
+        """turn message into a rendered image"""
+
         self.msg_image = self.font.render(
             msg, True, self.text_color, self.button_color
         )
@@ -27,5 +31,7 @@ class Button():
         self.msg_image_rect.center = self.rect.center
 
     def draw_button(self):
+        """drawning blank button and add message"""
+
         self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
