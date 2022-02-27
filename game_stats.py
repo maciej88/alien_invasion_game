5f36523edc4945825dfc7af9992d9ea2ec82b993
maciej <mkrywult88@gmail.com>
2022-02-27 16:52:06 +0100

    game statistic comments

--- game_stats.py
+++ game_stats.py
@@ -1,5 +1,6 @@
 class GameStats:
     """in game statistic"""
+
     def __init__(self, ai_game):
         self.settings = ai_game.settings
         self.reset_stats()
@@ -9,6 +10,8 @@ class GameStats:
         self.high_score = 0
 
     def reset_stats(self):
+        """statistics that can change during the game"""
+
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
